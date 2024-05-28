e724e0a74fb0cbc3a8afc968e1339439eac17e1e
Albert Fazlyev <albert.fz@yandex.ru>
2024-05-28 22:24:48 +0500

    fix determenism

--- data/train_test_split/split.py
+++ data/train_test_split/split.py
@@ -10,6 +10,7 @@ def stratified_split(
     seed: int|None = None
 ) -> list[pl.LazyFrame]:
     gen = np.random.default_rng(seed)
+    
 
     q = q.group_by(pl.exclude(x_cols), maintain_order=True).map_groups(
         lambda df: df.with_columns(
@@ -23,9 +24,11 @@ def stratified_split(
     )
 
     return [
-        q.filter(
+        q
+        .filter(
             pl.col('split_label') == i
         ).select(pl.exclude('split_label'))
+        .sort('text', maintain_order=True)
         for i in range(len(ratio))
     ]
 
@@ -35,10 +38,8 @@ def stratified_split(
 
 q = pl.scan_csv('data.csv')
 
-train, test, valid = pl.collect_all(
-    stratified_split(
-        q, [0.8, 0.1, 0.1], ['text'], seed=42
-    )
+train, test, valid = stratified_split(
+    q, [0.8, 0.1, 0.1], ['text'], seed=42
 )
 
 fig, axs = plt.subplots(ncols=len(train.columns), nrows=3, figsize=(190, 30),
@@ -48,20 +49,30 @@ fig, axs = plt.subplots(ncols=len(train.columns), nrows=3, figsize=(190, 30),
 colors = ['gray', 'yellow', 'green', 'red']
 
 
-for i, df in enumerate([train, test, valid]):
-    for j, column in enumerate(df.columns):
-        if column != 'text':
-            d = df[column].value_counts().rows_by_key(key=column)
-            d[0] = d.pop(None)
-            
-            
-            kv = [(int(k), list(v)[0]) for k, v in d.items()]
-            kv.sort(key=lambda x: x[0])
-            keys, values = zip(*kv)
+train = train.collect()
+test = test.collect()
+valid = valid.collect()
 
-            axs[i, j].pie(
-                values, labels=keys,
-                colors=colors
-            )
+def visualize():
+    for i, df in enumerate([train, test, valid]):
+        for j, column in enumerate(df.columns):
+            if column != 'text':
+                d = df[column].value_counts().rows_by_key(key=column)
+                d[0] = d.pop(None)
+                
+                
+                kv = [(int(k), list(v)[0]) for k, v in d.items()]
+                kv.sort(key=lambda x: x[0])
+                keys, values = zip(*kv)
 
-plt.show()
+                axs[i, j].pie(
+                    values, labels=keys,
+                    colors=colors
+                )
+
+    plt.show()
+
+
+train.write_csv('train.csv')
+test.write_csv('test.csv')
+valid.write_csv('valid.csv')
