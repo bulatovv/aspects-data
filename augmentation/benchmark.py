d20a50ff9d6f098296467c25968415d922c31bb3
Albert Fazlyev <albert.fz@yandex.ru>
2024-06-05 22:14:33 +0500

    implement paraphrase metrics

---
+++ augmentation/benchmark.py
@@ -0,0 +1,59 @@
+from evaluate import load
+from natasha import (
+    Segmenter,
+    MorphVocab, 
+    NewsEmbedding,
+    NewsMorphTagger,
+    Doc
+)
+import numpy as np
+import bert_score
+
+bertscore_metric = load('bertscore')
+bleu_metric = load('bleu')
+
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+
+
+def tokenize(text: str) -> list[str]:
+    doc = Doc(text)
+    doc.segment(segmenter)
+    doc.tag_morph(morph_tagger)
+
+    for token in doc.tokens: # type: ignore
+        token.lemmatize(morph_vocab)
+
+    return [
+        token.lemma for token in doc.tokens # type:ignore
+        if token.pos not in ['PUNCT', 'ADP', 'CCONJ']
+    ]
+
+def paraphrase_metrics(
+    references: list[str], 
+    candidates: list[str]
+) -> dict[str, float]:
+
+    bertscore = bertscore_metric.compute(
+        predictions=candidates, 
+        references=references,
+        model_type='DeepPavlov/rubert-base-cased',
+        num_layers=9
+    )['f1']
+    bleu = bleu_metric.compute(
+        predictions=candidates,
+        references=[[r] for r in references],
+        tokenizer=tokenize
+    )['bleu']
+
+    a = 1 - np.array(bleu)
+    b = np.array(bertscore)
+    combined = 2 * a * b / (a + b)
+
+    return {
+        'berstcore': np.average(bertscore),
+        'bleu': np.average(bleu),
+        'combined': np.average(combined) # type: ignore
+    }
