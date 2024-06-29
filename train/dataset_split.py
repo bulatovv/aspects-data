3fbcecb07e3e5d5db073dd56efc738c6a32910ad
daniil7 <daniil.k@mail.ru>
2024-06-29 20:14:07 +0500

    add fix dataset generation script

--- train/dataset_split.py
+++ train/dataset_split.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-dataset = pd.read_csv("dataset.csv", sep=",")
+dataset = pd.read_csv("datasets/dataset.csv", sep=",")
 
 train = dataset[(dataset["source"] == "train")
     | (dataset["source"] == "augmentation")
@@ -8,6 +8,6 @@ train = dataset[(dataset["source"] == "train")
 test = dataset[dataset["source"] == "test"]
 valid = dataset[dataset["source"] == "valid"]
 
-train.drop(columns=["source"]).to_csv("dataset_train.csv", sep=",", index=False)
-test.drop(columns=["source"]).to_csv("dataset_test.csv", sep=",", index=False)
-valid.drop(columns=["source"]).to_csv("dataset_valid.csv", sep=",", index=False)
+train.drop(columns=["source"]).to_csv("datasets/dataset_train.csv", sep=",", index=False)
+test.drop(columns=["source"]).to_csv("datasets/dataset_test.csv", sep=",", index=False)
+valid.drop(columns=["source"]).to_csv("datasets/dataset_valid.csv", sep=",", index=False)
